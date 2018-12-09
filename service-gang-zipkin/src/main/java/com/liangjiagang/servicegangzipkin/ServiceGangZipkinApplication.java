ea3547e531910b18025b0ff317769b4edaaf2a5e
liangjiagang <458468364@qq.com>
2018-12-09 21:37:21 +0800

    初次提交

--- service-gang-zipkin/src/main/java/com/liangjiagang/servicegangzipkin/ServiceGangZipkinApplication.java
+++ service-gang-zipkin/src/main/java/com/liangjiagang/servicegangzipkin/ServiceGangZipkinApplication.java
@@ -3,7 +3,7 @@ package com.liangjiagang.servicegangzipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
+import zipkin.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
