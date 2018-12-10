9fcdaedeaa64cb6508a0b5883a095fb8451da7d3
liangjiagang <458468364@qq.com>
2018-12-10 20:41:41 +0800

    初次提交

--- service-gang-web/src/main/java/com/liangjiagang/servicegangweb/controller/IndexController.java
+++ service-gang-web/src/main/java/com/liangjiagang/servicegangweb/controller/IndexController.java
@@ -5,15 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 
-    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/")
     public String hello(Model model) {
-        return "index";
+        model.addAttribute("textValue","hello woled");
+        return "/index-1";
+    }
+
+    @RequestMapping(value = "/index")
+    public ModelAndView index(){
+        return new ModelAndView("index-1");
     }
 
 
