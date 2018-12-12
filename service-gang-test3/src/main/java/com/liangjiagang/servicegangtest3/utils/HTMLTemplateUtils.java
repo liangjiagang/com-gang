12be209904bca49c04777c40aed1cbf1abe64e74
liangjiagang <458468364@qq.com>
2018-12-12 19:21:49 +0800

    初次提交

---
+++ service-gang-test3/src/main/java/com/liangjiagang/servicegangtest3/utils/HTMLTemplateUtils.java
@@ -0,0 +1,25 @@
+package com.liangjiagang.servicegangtest3.utils;
+/*import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;*/
+
+/**
+ * HTML模板渲染工具类
+ */
+public class HTMLTemplateUtils {
+
+
+    /**
+     * 使用 Thymeleaf 渲染 HTML
+     * @param template  HTML模板
+     * @param params 参数
+     * @return  渲染后的HTML
+     */
+    /*public static String render(String template, Map<String, Object> params){
+        Context context = new Context();
+        context.setVariables(params);
+        return templateEngine.process(template, context);
+    }*/
+
+}
